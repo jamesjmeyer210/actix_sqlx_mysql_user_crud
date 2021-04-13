9fb8c93dd49ab0df98af942861d84c780218c19a
James Meyer <jamesjmeyer210@gmail.com>
2021-04-13 10:03:22 -0500

    Removed commented code

--- src/dao/db_context.rs
+++ src/dao/db_context.rs
@@ -3,14 +3,6 @@ use sqlx::mysql::{MySqlRow, MySqlPoolOptions};
 use sqlx::{FromRow, MySqlPool};
 use std::sync::Arc;
 
-// pub struct Table<'c, T>
-// where
-//     T: FromRow<'c, MySqlRow<'c>>,
-// {
-//     pub pool: Arc<MySqlPool>,
-//     _from_row: fn(&MySqlRow<'c>) -> Result<T, sqlx::Error>,
-// }
-
 pub struct Table<'c, T>
     where
         T: FromRow<'c, MySqlRow>,
@@ -18,26 +10,6 @@ pub struct Table<'c, T>
     pub pool: Arc<MySqlPool>,
     _from_row: fn(&MySqlRow) -> Result<T, sqlx::Error>,
 }
-
-// pub struct Table<T>
-//     where
-//         T: FromRow<MySqlRow>,
-// {
-//     pub pool: Arc<MySqlPool>,
-//     _from_row: fn(&MySqlRow) -> Result<T, sqlx::Error>,
-// }
-
-// impl<'c, T> Table<'c, T>
-// where
-//     T: FromRow<'c, MySqlRow<'c>>,
-// {
-//     fn new(pool: Arc<MySqlPool>) -> Self {
-//         Table {
-//             pool,
-//             _from_row: T::from_row,
-//         }
-//     }
-// }
 
 impl<'c, T> Table<'c, T>
     where
@@ -51,30 +23,6 @@ impl<'c, T> Table<'c, T>
     }
 }
 
-// impl<T> Table<T>
-//     where
-//         T: FromRow<MySqlRow>,
-// {
-//     fn new(pool: Arc<MySqlPool>) -> Self {
-//         Table {
-//             pool,
-//             _from_row: T::from_row,
-//         }
-//     }
-// }
-
-// pub struct JoinTable<'c, T1, T2>
-// where
-//     T1: FromRow<'c, MySqlRow<'c>>,
-//     T2: FromRow<'c, MySqlRow<'c>>,
-// {
-//     pub pool: Arc<MySqlPool>,
-//     _from_row: (
-//         fn(&MySqlRow<'c>) -> Result<T1, sqlx::Error>,
-//         fn(&MySqlRow<'c>) -> Result<T2, sqlx::Error>,
-//     ),
-// }
-
 pub struct JoinTable<'c, T1, T2>
     where
         T1: FromRow<'c, MySqlRow>,
@@ -86,31 +34,6 @@ pub struct JoinTable<'c, T1, T2>
         fn(&MySqlRow) -> Result<T2, sqlx::Error>,
     ),
 }
-
-// pub struct JoinTable<T1, T2>
-//     where
-//         T1: FromRow<MySqlRow>,
-//         T2: FromRow<MySqlRow>,
-// {
-//     pub pool: Arc<MySqlPool>,
-//     _from_row: (
-//         fn(&MySqlRow) -> Result<T1, sqlx::Error>,
-//         fn(&MySqlRow) -> Result<T2, sqlx::Error>,
-//     ),
-// }
-
-// impl<'c, T1, T2> JoinTable<'c, T1, T2>
-// where
-//     T1: FromRow<'c, MySqlRow<'c>>,
-//     T2: FromRow<'c, MySqlRow<'c>>,
-// {
-//     fn new(pool: Arc<MySqlPool>) -> Self {
-//         JoinTable {
-//             pool,
-//             _from_row: (T1::from_row, T2::from_row),
-//         }
-//     }
-// }
 
 impl<'c, T1, T2> JoinTable<'c, T1, T2>
     where
@@ -124,25 +47,6 @@ impl<'c, T1, T2> JoinTable<'c, T1, T2>
         }
     }
 }
-
-// impl<T1, T2> JoinTable<T1, T2>
-//     where
-//         T1: FromRow<MySqlRow>,
-//         T2: FromRow<MySqlRow>,
-// {
-//     fn new(pool: Arc<MySqlPool>) -> Self {
-//         JoinTable {
-//             pool,
-//             _from_row: (T1::from_row, T2::from_row),
-//         }
-//     }
-// }
-
-// pub struct Database<'c> {
-//     pub users: Arc<Table<'c, User>>,
-//     pub groups: Arc<Table<'c, Group>>,
-//     pub users_to_groups: Arc<JoinTable<'c, User, Group>>,
-// }
 
 pub struct Database<'c> {
     pub users: Arc<Table<'c, User>>,
