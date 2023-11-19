3e4232c9ec8627c92fb6be210fb8a3064eadc4d6
James Meyer <jamesjmeyer210@gmail.com>
2023-11-19 16:03:37 -0700

    updated db context

--- src/dao/db_context.rs
+++ src/dao/db_context.rs
@@ -1,23 +1,23 @@
 use super::{Group, User};
-use sqlx::mysql::MySqlRow;
-use sqlx::{FromRow, MySqlPool};
+use sqlx::{FromRow, SqlitePool};
 use std::marker::PhantomData;
 use std::sync::Arc;
+use sqlx::sqlite::SqliteRow;
 
 pub struct Table<'c, T>
 where
-    T: FromRow<'c, MySqlRow>,
+    T: FromRow<'c, SqliteRow>,
 {
-    pub pool: Arc<MySqlPool>,
-    _from_row: fn(&'c MySqlRow) -> Result<T, sqlx::Error>,
+    pub pool: Arc<SqlitePool>,
+    _from_row: fn(&'c SqliteRow) -> Result<T, sqlx::Error>,
     _marker: PhantomData<&'c T>,
 }
 
 impl<'c, T> Table<'c, T>
 where
-    T: FromRow<'c, MySqlRow>,
+    T: FromRow<'c, SqliteRow>,
 {
-    fn new(pool: Arc<MySqlPool>) -> Self {
+    fn new(pool: Arc<SqlitePool>) -> Self {
         Table {
             pool,
             _from_row: T::from_row,
@@ -28,13 +28,13 @@ where
 
 pub struct JoinTable<'c, T1, T2>
 where
-    T1: FromRow<'c, MySqlRow>,
-    T2: FromRow<'c, MySqlRow>,
+    T1: FromRow<'c, SqliteRow>,
+    T2: FromRow<'c, SqliteRow>,
 {
-    pub pool: Arc<MySqlPool>,
+    pub pool: Arc<SqlitePool>,
     _from_row: (
-        fn(&'c MySqlRow) -> Result<T1, sqlx::Error>,
-        fn(&'c MySqlRow) -> Result<T2, sqlx::Error>,
+        fn(&'c SqliteRow) -> Result<T1, sqlx::Error>,
+        fn(&'c SqliteRow) -> Result<T2, sqlx::Error>,
     ),
     _marker_t1: PhantomData<&'c T1>,
     _marker_t2: PhantomData<&'c T2>,
@@ -42,10 +42,10 @@ where
 
 impl<'c, T1, T2> JoinTable<'c, T1, T2>
 where
-    T1: FromRow<'c, MySqlRow>,
-    T2: FromRow<'c, MySqlRow>,
+    T1: FromRow<'c, SqliteRow>,
+    T2: FromRow<'c, SqliteRow>,
 {
-    fn new(pool: Arc<MySqlPool>) -> Self {
+    fn new(pool: Arc<SqlitePool>) -> Self {
         JoinTable {
             pool,
             _from_row: (T1::from_row, T2::from_row),
@@ -63,7 +63,7 @@ pub struct Database<'c> {
 
 impl<'a> Database<'a> {
     pub async fn new(sql_url: &String) -> Database<'a> {
-        let connection = MySqlPool::connect(&sql_url).await.unwrap();
+        let connection = SqlitePool::connect(&sql_url).await.unwrap();
         let pool = Arc::new(connection);
 
         Database {
